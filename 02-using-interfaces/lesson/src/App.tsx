e0066f57b38c2c60c803a74b48c25d3322abfd39
Braedon Gough <braedon@pleo.io>
2020-08-22 13:59:38 +0200

    Interfaces

--- 02-using-interfaces/lesson/src/App.tsx
+++ 02-using-interfaces/lesson/src/App.tsx
@@ -1,29 +1,36 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
 
-// todo: Try to add a prop not defined in the interface
-// todo: Omit a prop defined in the interface
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface Dog {
+    numberOfEyes: number
+    color: string
+    tail: boolean
 }
 
-export default App;
+interface Props {
+    dog: Dog
+}
+
+function App({ dog }: Props) {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    {dog.color}
+                </a>
+            </header>
+        </div>
+    )
+}
+
+export default App
