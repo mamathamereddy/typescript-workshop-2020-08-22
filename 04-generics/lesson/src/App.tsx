d82373a21db46e65ba55b69a52b865f849500036
Braedon Gough <braedon@pleo.io>
2020-08-22 15:14:22 +0200

    Lesson 4

--- 04-generics/lesson/src/App.tsx
+++ 04-generics/lesson/src/App.tsx
@@ -1,47 +1,57 @@
-import React, {ChangeEvent} from 'react'
+import React, { ChangeEvent } from 'react'
 import logo from './logo.svg'
 import './App.css'
 
-interface Electric {
-  sizeOfBattery: string
-  range: number
-  getNumberOfRecharges: () => number
+export interface Electric {
+    sizeOfBattery: string
+    range: number
+    getNumberOfRecharges: (numberOfCharges: number) => number
 }
 
-interface Gas {
-  kmPerLiter: number
-  typeOfFuel: string // diesel or petrol
-  getDateOfLastOilChange: () => string
+export interface Gas {
+    kmPerLiter: number
+    typeOfFuel: string // diesel or petrol
+    getDateOfLastOilChange: () => string
 }
 
-interface Vehicle<TypeOfVehicle> {
-  color: string
-  topSpeed: number
-  registerNewOwner: (ownerName: string) => void
-  specs: TypeOfVehicle
+export interface Vehicle<TypeOfVehicle> {
+    color: string
+    topSpeed: number
+    registerNewOwner: (ownerName: string) => void
+    specs: TypeOfVehicle
 }
 
-// interface Props {
-//   vehicle: Vehicle
-// }
+interface Props {
+    vehicle: Vehicle<Electric>
+}
 
-const App = () => {
-  // todo: Implement the handle change method
-  // const [value, setValue] = React.useState('')
+const App = (props: Props) => {
+    const [value, setValue] = React.useState('')
 
-  // const handleChange = (event:) => {
-  //   setValue(event.target.value)
-  // }
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </header>
-    </div>
-  )
+    // const onChange = (event: ChangeEvent<HTMLInputElement>) => {
+    //     setValue(event.target.value)
+    // }
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                sizeOfBattery: {props.vehicle.specs.sizeOfBattery}
+                <input
+                    type="text"
+                    placeholder="really good input"
+                    value={value}
+                    onChange={(event) => setValue(event.target.value)}
+                />
+            </header>
+        </div>
+    )
 }
 
 export default App
+// todo: Implement the handle change method
+// const [value, setValue] = React.useState('')
+
+// const handleChange = (event:) => {
+//   setValue(event.target.value)
+// }
--- 04-generics/lesson/src/index.tsx
+++ 04-generics/lesson/src/index.tsx
@@ -1,17 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
+import { Vehicle, Electric, Gas } from './App'
+
+const gasSpecs = {
+    kmPerLiter: 39,
+    typeOfFuel: 'diesel', // diesel or petrol
+    getDateOfLastOilChange: () => 'YESTERDAY',
+}
+
+const electricSpecs: Electric = {
+    sizeOfBattery: '100',
+    range: 9999999,
+    getNumberOfRecharges: (numberOfCharges: number) => numberOfCharges,
+}
+
+const vehicle = {
+    topSpeed: 399,
+    color: 'pink',
+    registerNewOwner: () => 'BRAEDON OWNS THIS NOW',
+    specs: electricSpecs,
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    <React.StrictMode>
+        <App vehicle={vehicle} />
+    </React.StrictMode>,
+    document.getElementById('root')
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
