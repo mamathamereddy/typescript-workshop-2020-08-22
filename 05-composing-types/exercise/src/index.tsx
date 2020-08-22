e228ae02feacd2d750cbb129d89490c7c2e7a1f4
Braedon Gough <braedon@pleo.io>
2020-08-22 16:57:47 +0200

    excersie 5

--- 05-composing-types/exercise/src/index.tsx
+++ 05-composing-types/exercise/src/index.tsx
@@ -2,64 +2,72 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const myPets = [
-  {lovesToBark: false, name: 'Spot', favoriteFetchToy: 'tennis ball'},
-  {lovesToBark: true, name: 'Penny', favoriteFetchToy: 'tree branch'},
-  {firstName: 'Teddy', drinksMilk: true, favoriteNappingSpot: 'on the bed'},
+    { lovesToBark: false, name: 'Spot', favoriteFetchToy: 'tennis ball' },
+    { lovesToBark: true, name: 'Penny', favoriteFetchToy: 'tree branch' },
+    { firstName: 'Teddy', drinksMilk: true, favoriteNappingSpot: 'on the bed' },
 ]
 
 interface Dog {
-  lovesToBark: boolean
-  name: string
-  favoriteFetchToy: string
+    lovesToBark: boolean
+    name: string
+    favoriteFetchToy: string
 }
 
 interface Cat {
-  drinksMilk: boolean
-  firstName: string
-  favoriteNappingSpot: string
+    drinksMilk: boolean
+    firstName: string
+    favoriteNappingSpot: string
 }
 
-type Pet = //
+type Pet = Cat | Dog
 
 interface CatProps {
-  cat: Cat
+    cat: Cat
 }
 
-const Cat = ({cat}: CatProps) => (
-  <div style={{border: '1px solid blue'}}>
-    <div>Name: {cat.firstName}</div>
-    <div>Drinks milk: {cat.drinksMilk ? '✅' : '❌'}</div>
-    <div>You can find them sleeping here: {cat.favoriteNappingSpot}</div>
-  </div>
+const Cat = ({ cat }: CatProps) => (
+    <div style={{ border: '1px solid blue' }}>
+        <div>Name: {cat.firstName}</div>
+        <div>Drinks milk: {cat.drinksMilk ? '✅' : '❌'}</div>
+        <div>You can find them sleeping here: {cat.favoriteNappingSpot}</div>
+    </div>
 )
 
 interface DogProps {
-  dog: Dog
+    dog: Dog
 }
 
-const Dog = ({dog}: DogProps) => (
-  <div style={{border: '1px solid red'}}>
-    <div>Name: {dog.name}</div>
-    <div>Drinks milk: {dog.lovesToBark ? '✅' : '❌'}</div>
-    <div>Wann play fetch? Make sure you bring a {dog.favoriteFetchToy}! </div>
-  </div>
+const Dog = ({ dog }: DogProps) => (
+    <div style={{ border: '1px solid red' }}>
+        <div>Name: {dog.name}</div>
+        <div>Drinks milk: {dog.lovesToBark ? '✅' : '❌'}</div>
+        <div>
+            Wann play fetch? Make sure you bring a {dog.favoriteFetchToy}!{' '}
+        </div>
+    </div>
 )
 
 interface AppProps {
-  pets: Pet[]
+    pets: Pet[]
 }
 
-const PetList = ({pets}: AppProps) => {
-  return (
-    <div style={{display: 'grid', gap: 10}}>
-      {/* solution goes here */})}
-    </div>
-  )
+const PetList = ({ pets }: AppProps) => {
+    return (
+        <div style={{ display: 'grid', gap: 10 }}>
+            {pets.map((pet) => {
+                if ('firstName' in pet) {
+                    return <Cat cat={pet} />
+                } else {
+                    return <Dog dog={pet} />
+                }
+            })}
+        </div>
+    )
 }
 
 ReactDOM.render(
-  <React.StrictMode>
-    <PetList pets={myPets} />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <PetList pets={myPets} />
+    </React.StrictMode>,
+    document.getElementById('root')
 )
